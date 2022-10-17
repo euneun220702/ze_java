c6ef7133b3af97de7609d0b7ad315ab9254b52bd
euneun220702 <108576327+euneun220702@users.noreply.github.com>
2022-10-17 21:49:08 +0900

    Update seven_lotto.java

--- seven_lotto.java
+++ seven_lotto.java
@@ -63,3 +63,47 @@ System.out.println("[로또 당첨 프로그램]");
 
 }
 }
+
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class test {
+        public static void main(String[] args) {
+            System.out.println("[로또 당첨 프로그램]");
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("로또 개수를 입력해 주세요.(숫자 1~10): ");
+            int number = scanner.nextInt();
+
+            HashSet set = new HashSet();
+
+
+            for (int a = 1; a <= number; a++) {
+
+                    for (int i = 0; set.size() < 6; i++) {
+                        int num = (int) (Math.random() * 45) + 1;
+                        set.add(num);
+                    }
+
+                    System.out.print((char) (65 + a - 1) + " ");
+                    LinkedList list = new LinkedList(set);
+                    Collections.sort(list);
+                    System.out.println(list);
+                }
+            System.out.println();
+            System.out.println("[로또 발표]");
+            HashSet set2 = new HashSet();
+            for (int i = 0; set2.size() < 6; i++) {
+                int num = (int) (Math.random() * 45) + 1;
+                set2.add(num);
+            }
+            System.out.print("  ");
+            LinkedList list2 = new LinkedList(set2);
+            Collections.sort(list2);
+            System.out.println(list2);
+
+        }
+    }
